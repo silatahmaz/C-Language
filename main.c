1edbaa50536f4c99525dfcacffaa87942cf60942
silatahmaz <133128482+silatahmaz@users.noreply.github.com>
2023-09-19 12:55:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) 
+{ 
+	int integer;
+	printf("Please enter an integer");
+	scanf("%d", &integer);
+	printf("Your integer is %d", integer);
+	return 0;
+}
---
+++ operatrs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) 
+{
+	int num1= 5;
+	int num2=6;
+	int addition = num1 + num2;
+	int substraction = num1 - num2;
+	float division = num1 / num2 ;
+	int multiplication = num11 * num2;
+	int modulus= num1 % num2;
+	int increment = num1++;
+	int decrement= num1--;
+	
+	if(num1==num2) //You can test equality of this two integer
+	if(num1>=num2) //You can test whether num1 is greater than or equal to num2
+	if(num1<=num2) //You can test whether num1 is less than or equal to num2
+	
+	return 0;
+}
